652eeac24b281a126167861833c4959a9bc5fcf0
Laljanibasha Shaik <98688990+laljohnny@users.noreply.github.com>
2025-12-16 17:09:47 +0530

    Remove variables from variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,1 @@
-variable "location" {
-  default = "East US"
-}
 
-variable "vm_name" {
-  default = "demo-vm"
-}
-
-variable "admin_username" {
-  default = "azureuser"
-}
-
-variable "ssh_public_key" {
-  description = "SSH public key path"
-}
