2608b01f9f7e394398e6e44c689385e3b05e30ef
Laljanibasha Shaik <98688990+laljohnny@users.noreply.github.com>
2025-12-17 16:09:33 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name  = "c2c-info-tech"
+    storage_account_name = "cloudskillacademy"
+    container_name       = "terraformstate"
+    key                  = "prod.tfstate"
+  }
 }
