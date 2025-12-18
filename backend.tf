e86b263a49232e72a805ab33c45351aac6d5b113
Laljanibasha Shaik <98688990+laljohnny@users.noreply.github.com>
2025-12-18 11:10:03 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "c2c-info-tech"
     storage_account_name = "cloudskillacademy"
     container_name       = "terraformstate"
-    key                  = "prod.tfstate"
+    key                  = "C99Zb4gewZQq9Y1wp33tdjxkY6znUKdfRmn6siQro/wZg8OTkzpKsQfe+KEXq5Vyg3clswFXkLkc+AStvvVZfA=="
   }
 }
