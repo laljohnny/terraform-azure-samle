99a5d4619c4fdfd6ab65cf4f224a50c9d434048e
Laljanibasha Shaik <98688990+laljohnny@users.noreply.github.com>
2025-12-18 20:33:23 +0530

    Update azurerm provider version to 4.8.0

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      source = "hashicorp/azurerm"
+      version = "4.8.0"
     }
   }
 }
@@ -10,5 +10,3 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
-
